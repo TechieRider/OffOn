807ff2ac869e56871f14cc48c3e599737660e36d
J-ScrumMaster <rugbyjohnny@hotmail.com>
2015-02-02 15:23:26 +0100

    The projects in

---
+++ ThisProject/ThisProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThisProject.Startup))]
+namespace ThisProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
